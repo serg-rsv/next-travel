5b5be2b66fd2afcb50eaba48ae2d58734f8c45bc
Serhii Rybak <rybaksw@gmail.com>
2023-05-18 13:19:59 +0300

    fix: Main title size

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,9 +14,10 @@ export default function Hero() {
         alt="Зелені острови"
       />
       <div className="absolute top-0 left-0 w-full">
-        <div className="container flex flex-col w-full mx-auto pt-24 px-5 lg:px-16 xl:px-36">
-          <h1 className="mb-4 sm:w-1/2 md:w-1/2 text-2xl md:text-4xl xl:text-5xl font-bold uppercase text-white">
-            Життя коротке, а світ безмежний
+        <div className="container flex flex-col w-full mx-auto pt-20 px-5 lg:px-16 xl:px-36">
+          <h1 className="mb-4 text-xl drop-shadow-md md:text-4xl xl:text-5xl font-bold uppercase text-white">
+            Життя коротке,
+            <br /> а світ безмежний
           </h1>
           <Link
             className="self-baseline px-4 py-2 border rounded-md bg-white bg-opacity-50 hover:bg-opacity-75 font-semibold md:text-lg xl:text-2xl"
