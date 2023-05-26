e0dff129c7fa2696b46e3b5b46e9578b3c3ae1df
Serhii Rybak <rybaksw@gmail.com>
2023-05-26 17:10:01 +0300

    fix: input dark theme

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -14,7 +14,7 @@ export default function Input({
         id={id}
         className={`input peer placeholder:text-transparent w-full px-2 py-1 bg-transparent border-b-2 ${
           error ? 'border-red-500' : 'border-gray-300'
-        } focus:border-blue-500 focus:outline-none text-gray-950`}
+        } focus:border-blue-500 focus:outline-none text-gray-950 dark:text-white`}
         type={type}
         placeholder={placeholder}
         autoComplete="none"
@@ -22,7 +22,7 @@ export default function Input({
       />
       <label
         htmlFor={id}
-        className="absolute text-sm transition-all -top-4 left-2 peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-1 peer-placeholder-shown:text-base peer-focus:text-gray-900 peer-focus:-top-4 peer-focus:text-sm"
+        className="absolute text-sm transition-all -top-4 left-2 dark:text-gray-200 peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-1 peer-placeholder-shown:text-base peer-focus:text-gray-900 dark:peer-focus:text-gray-200 peer-focus:-top-4 peer-focus:text-sm"
       >
         {label}
       </label>
